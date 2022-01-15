865684acefca11e1532f9cb112802ef612c53b8c
João Paulo <jptick@gmail.com>
2022-01-15 09:09:05 -0300

    Init BackEnd Project

---
+++ backend/src/main/java/com/supportportal/SupportportalApplication.java
@@ -0,0 +1,13 @@
+package com.supportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportportalApplication.class, args);
+	}
+
+}
