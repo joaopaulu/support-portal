1a01ab2ff714d876973d611d9da76630e0a26ba5
João Paulo <jptick@gmail.com>
2022-01-15 12:07:37 -0300

    Implementing Application Domain User

---
+++ backend/src/main/java/com/supportportal/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.supportportal.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @GetMapping
+    public String showUser(){
+        return "application Works";
+    }
+}
