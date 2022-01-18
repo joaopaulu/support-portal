aa0134ce2935978d30744ccacc712839c582cb24
João Paulo Lima <jptick@gmail.com>
2022-01-18 13:08:41 -0300

    Update JWT Token Provider

--- backend/src/main/java/com/supportportal/utils/JWTTokenProvider.java
+++ backend/src/main/java/com/supportportal/utils/JWTTokenProvider.java
@@ -1,16 +1,24 @@
 package com.supportportal.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.supportportal.entities.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.supportportal.constant.SecurityConstant.*;
+import static java.util.Arrays.stream;
 
 public class JWTTokenProvider {
-    @Value("jwt.secret")
+    @Value("${jwt.secret}")
     private String secret;
 
     public String generateJwtToken(UserPrincipal userPrincipal){
@@ -21,7 +29,32 @@ public class JWTTokenProvider {
                 .sign(Algorithm.HMAC512(secret.getBytes()));
     }
 
-    public String[] getClaimsFromUser(UserPrincipal userPrincipal){
-        return null;
+    public List<GrantedAuthority> getAuthorities(String token){
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private String[] getClaimsFromToken(String token){
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier(){
+        JWTVerifier verifier;
+        try{
+            Algorithm algorithm = Algorithm.HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(GET_ARRAYS_LLC).build();
+        }catch (JWTVerificationException exception){
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
+
+    public String[] getClaimsFromUser(UserPrincipal user){
+        List<String> authorithies = new ArrayList<>();
+        for(GrantedAuthority grantedAuthority : user.getAuthorities()){
+            authorithies.add(grantedAuthority.getAuthority());
+        }
+        return authorithies.toArray(new String[0]);
     }
 }
