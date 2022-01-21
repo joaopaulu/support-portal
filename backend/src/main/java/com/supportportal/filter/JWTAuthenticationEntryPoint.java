3f977c07781f89bf8797c9d45e7773f0b5c01854
João Paulo <jptick@gmail.com>
2022-01-21 19:40:08 -0300

    Implementing Access Denied Handler

--- backend/src/main/java/com/supportportal/filter/JWTAuthenticationEntryPoint.java
+++ backend/src/main/java/com/supportportal/filter/JWTAuthenticationEntryPoint.java
@@ -14,7 +14,7 @@ import java.io.OutputStream;
 import static com.supportportal.constant.SecurityConstant.FORBIDDEN_MESSAGE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+public class JWTAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
