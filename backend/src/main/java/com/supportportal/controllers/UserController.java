ebad32287e4ad82bb3a80d4b85ae42737bc19a94
João Paulo Lima <jptick@gmail.com>
2022-01-24 11:38:52 -0300

    Create Custom Exception Handling

--- backend/src/main/java/com/supportportal/controllers/UserController.java
+++ backend/src/main/java/com/supportportal/controllers/UserController.java
@@ -1,12 +1,13 @@
 package com.supportportal.controllers;
 
+import com.supportportal.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
-public class UserController {
+public class UserController extends ExceptionHandling {
 
     @GetMapping
     public String showUser(){
---
+++ backend/src/main/java/com/supportportal/exception/domain/ExceptionHandling.java
@@ -0,0 +1,37 @@
+package com.supportportal.exception.domain;
+
+import com.supportportal.entities.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Locale;
+
+@RestControllerAdvice
+public class ExceptionHandling {
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration";
+    private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request";
+    private static final String INTERNAL_SERVER_ERROR_MSG = "An error occurred while processing the request";
+    private static final String INCORRECT_CREDENTIALS = "Username / password incorrect. Please try again";
+    private static final String ACCOUNT_DISABLED = "Your account has been disabled. If this is an error, please contact administration";
+    private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
+    private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+    public static final String ERROR_PATH = "/error";
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> accountDisabledException(){
+        return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_DISABLED);
+    }
+
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message){
+       HttpResponse httpResponse = new HttpResponse(httpStatus.value(), httpStatus,
+               httpStatus.getReasonPhrase().toUpperCase(), message.toUpperCase());
+       return new ResponseEntity<>(httpResponse, httpStatus);
+    }
+
+}
