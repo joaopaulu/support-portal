b9589920a4cf96687b85ac0dde542f76a4127e75
João Paulo <jptick@gmail.com>
2022-01-30 16:25:34 -0300

    Brute Force Attack Cache

---
+++ backend/src/main/java/com/supportportal/service/LoginAttemptService.java
@@ -0,0 +1,25 @@
+package com.supportportal.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+@Service
+public class LoginAttemptService {
+    private static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
+    private static final int ATTEMPT_INCREMENT = 1;
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    public LoginAttemptService() {
+        super();
+        loginAttemptCache = CacheBuilder.newBuilder().expireAfterWrite(15, MINUTES)
+                .maximumSize(100).build(new CacheLoader<String, Integer>() {
+                    public Integer load(String key) {
+                        return 0;
+                    }
+                });
+    }
+}
