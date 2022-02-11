ed7672a4d746f0eb479aab36a76e0c7933303737
João Paulo <jptick@gmail.com>
2022-02-11 17:15:42 -0300

    Update LoginAttemptCache

--- backend/src/main/java/com/supportportal/listener/AuthenticationSuccessListener.java
+++ backend/src/main/java/com/supportportal/listener/AuthenticationSuccessListener.java
@@ -1,6 +1,7 @@
 package com.supportportal.listener;
 
 import com.supportportal.domain.User;
+import com.supportportal.domain.UserPrincipal;
 import com.supportportal.service.LoginAttemptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -19,8 +20,8 @@ public class AuthenticationSuccessListener {
     @EventListener
     public void onAuthenticationSuccess(AuthenticationSuccessEvent event){
         Object principal = event.getAuthentication().getPrincipal();
-        if(principal instanceof User){
-            User user = (User) event.getAuthentication().getPrincipal();
+        if(principal instanceof UserPrincipal){
+            UserPrincipal user = (UserPrincipal) event.getAuthentication().getPrincipal();
             loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
         }
     }
