5ab03ae599179ee17aa7b0aa815fa1c61488f0ef
João Paulo <jptick@gmail.com>
2022-02-13 14:48:01 -0300

    Implementing File Constants

--- backend/src/main/java/com/supportportal/service/UserService.java
+++ backend/src/main/java/com/supportportal/service/UserService.java
@@ -4,6 +4,7 @@ import com.supportportal.domain.User;
 import com.supportportal.exception.domain.EmailExistException;
 import com.supportportal.exception.domain.UserNotFoundException;
 import com.supportportal.exception.domain.UsernameExistException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.util.List;
@@ -17,4 +18,14 @@ public interface UserService {
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firsName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String NewEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    void deleteUser(long id);
+
+    void resetPassword(String email);
+
+    User updateProfileImage(String username, MultipartFile profileImage);
 }
