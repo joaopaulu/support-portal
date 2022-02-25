65fd5cbe9fdb1a282818d7307e9b2018a0e8162a
João Paulo <jptick@gmail.com>
2022-02-25 19:13:32 -0300

    Create UserService Methods

--- frontend/src/app/service/authentication.service.ts
+++ frontend/src/app/service/authentication.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {environment} from 'environments/environment';
 import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {User} from '../model/user';
+import {User} from 'app/model/user';
 import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from 'environments/environment';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from 'app/model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private host = environment.apiUrl + '/user';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}/list`);
+  }
+
+  public addUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/add`, formData);
+  }
+
+  public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
+    return this.http.post<User>(`${this.host}/update`, formData);
+  }
+
+  public resetPassword(email: string): Observable<any | HttpErrorResponse> {
+    return this.http.get(`${this.host}/resetpassword/${email}`);
+  }
 }
