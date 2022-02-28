9992d30004bd60e5e29196af97277d812ed0c245
João Paulo <jptick@gmail.com>
2022-02-28 20:34:13 -0300

    Implementing Login Component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../service/authentication.service';
+import {NotificationService} from '../service/notification.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private router: Router, private authenticationService: AuthenticationService,
+              private notificationService: NotificationService) {
+  }
 
   ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigateByUrl('/user/management');
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
