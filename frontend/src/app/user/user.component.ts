75c7dc17b65e0333b89fee7e155fdf74061ea8e2
João Paulo <jptick@gmail.com>
2022-03-04 18:41:26 -0300

    Implementing Edit User Form Modal

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -4,8 +4,11 @@ import {User} from 'app/model/user';
 import {UserService} from 'app/service/user.service';
 import {NotificationService} from 'app/service/notification.service';
 import {NotificationType} from 'app/enum/notification-type.enum';
-import {HttpErrorResponse} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpEventType} from '@angular/common/http';
 import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthenticationService} from 'app/service/authentication.service';
+import {FileUploadStatus} from 'app/model/file-upload.status';
 
 @Component({
   selector: 'app-user',
@@ -16,13 +19,18 @@ export class UserComponent implements OnInit {
   private titleSubject = new BehaviorSubject<string>('Users');
   public titleAction$ = this.titleSubject.asObservable();
   public users: User[];
+  public user: User;
   public refreshing: boolean;
   public selectedUser: User;
   public fileName: string;
   public profileImage: File;
   private subscriptions: Subscription[] = [];
+  public editUser = new User();
+  private currentUsername: string;
+  public fileStatus = new FileUploadStatus();
 
-  constructor(private userService: UserService, private notificationService: NotificationService) {
+  constructor(private router: Router, private authenticationService: AuthenticationService,
+              private userService: UserService, private notificationService: NotificationService) {
   }
 
   ngOnInit(): void {
@@ -87,6 +95,89 @@ export class UserComponent implements OnInit {
     );
   }
 
+  public onUpdateUser(): void {
+    const formData = this.userService.createUserFormDate(this.currentUsername, this.editUser, this.profileImage);
+    this.subscriptions.push(
+      this.userService.updateUser(formData).subscribe(
+        (response: User) => {
+          this.clickButton('closeEditUserModalButton');
+          this.getUsers(false);
+          this.fileName = null;
+          this.profileImage = null;
+          this.sendNotification(NotificationType.SUCCESS, `${response.firstName} ${response.lastName} updated successfully`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.profileImage = null;
+        }
+      )
+    );
+  }
+
+  public onUpdateCurrentUser(user: User): void {
+    this.refreshing = true;
+    this.currentUsername = this.authenticationService.getUserFromLocalCache().username;
+    const formData = this.userService.createUserFormDate(this.currentUsername, user, this.profileImage);
+    this.subscriptions.push(
+      this.userService.updateUser(formData).subscribe(
+        (response: User) => {
+          this.authenticationService.addUserToLocalCache(response);
+          this.getUsers(false);
+          this.fileName = null;
+          this.profileImage = null;
+          this.sendNotification(NotificationType.SUCCESS, `${response.firstName} ${response.lastName} updated successfully`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.refreshing = false;
+          this.profileImage = null;
+        }
+      )
+    );
+  }
+
+  public onUpdateProfileImage(): void {
+    const formData = new FormData();
+    formData.append('username', this.user.username);
+    formData.append('profileImage', this.profileImage);
+    this.subscriptions.push(
+      this.userService.updateProfileImage(formData).subscribe(
+        (event: HttpEvent<any>) => {
+          this.reportUploadProgress(event);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.fileStatus.status = 'done';
+        }
+      )
+    );
+  }
+
+  private reportUploadProgress(event: HttpEvent<any>): void {
+    switch (event.type) {
+      case HttpEventType.UploadProgress:
+        this.fileStatus.percentage = Math.round(100 * event.loaded / event.total);
+        this.fileStatus.status = 'progress';
+        break;
+      case HttpEventType.Response:
+        if (event.status === 200) {
+          this.user.profileImageUrl = `${event.body.profileImageUrl}?time=${new Date().getTime()}`;
+          this.sendNotification(NotificationType.SUCCESS, `${event.body.firstName}\'s profile image updated successfully`);
+          this.fileStatus.status = 'done';
+          break;
+        } else {
+          this.sendNotification(NotificationType.ERROR, `Unable to upload image. Please try again`);
+          break;
+        }
+      // tslint:disable-next-line:no-unused-expression
+      default: 'Finished all processes';
+    }
+  }
+
+  public updateProfileImage(): void {
+    this.clickButton('profile-image-input');
+  }
+
   public searchUsers(searchTerm: string): void {
     const results: User[] = [];
     for (const user of this.userService.getUsersToLocalCache()) {
@@ -103,6 +194,12 @@ export class UserComponent implements OnInit {
     if (results.length === 0 || !searchTerm) {
       this.users = this.userService.getUsersToLocalCache();
     }
+  }
+
+  public onEditUser(editUser: User): void {
+    this.editUser = editUser;
+    this.currentUsername = editUser.username;
+    this.clickButton('openUserEdit');
   }
 
   // tslint:disable-next-line:typedef
